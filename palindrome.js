4e4d1cdecdac6d8907f1e420657637dca7fb39a1
Leonard <leonard.flachs@gmail.com>
2020-02-10 14:23:26 +0100

    first commit

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+// Write a method called "palindrome" that checks if a given string is palindrome or not, and return true or false.
+
+const palindrome = (text) => {
+
+
+};
+console.log(palindrome("hello/^?/wo\ rld"));
+module.exports = palindrome; 
