d1ba91dae5db87d0274c365046e74b7a9bbc46df
Leonard <leonard.flachs@gmail.com>
2020-02-10 14:25:00 +0100

    added test modules

---
+++ test/palindrome.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const palindrome = require('../palindrome.js');
+
+describe('palindrome test', () => {
+    it('should be loaded', () => assert.equal(true, true));
+
+    it('Should be true with "Racecar"', () => assert.equal(palindrome("Racecar"), true));
+
+    it('Should be true with "Won\'t cat lovers revolt ? Act now!"', () => assert.equal(palindrome("Won't cat lovers revolt? Act now!"), true));
+
+    it('Should be false with "hello world"', () => assert.equal(palindrome("hello world"), false));
+});
